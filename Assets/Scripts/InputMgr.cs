9bd3b8fbacefbfecf1fbc69a7429c11c621ce824
陈昱皓 <380274661@qq.com>
2018-03-31 14:27:43 +0800

    增加了画笔范围提示

--- Assets/Scripts/InputMgr.cs
+++ Assets/Scripts/InputMgr.cs
@@ -12,6 +12,8 @@ namespace Painter
         public static bool IsUI { get; private set; }
         public static IInput Target { get; private set; }
         private Vector3 last;
+        [SerializeField]
+        private Material mat;
         private void Update()
         {
             if (Input.GetMouseButtonDown(0))
@@ -53,6 +55,22 @@ namespace Painter
                 IsUI = false;
             }
         }
+        private void OnRenderObject()
+        {
+            if (!IsUI)
+            {
+                RaycastHit h = MainRay(Input.mousePosition);
+                if (h.transform)
+                {
+                    if (h.transform.GetComponent<IInput>() != null)
+                    {
+                        mat.SetColor("_Color",Painter.Color);
+                        DrawWrieCircle(h.point, Quaternion.LookRotation(h.normal), mat, Painter.Size * 1e-3f);
+                        DrawWrieLine(h.point, h.point + h.normal * Painter.Size * 1e-3f, mat);
+                    }
+                }
+            }
+        }
         public static RaycastHit MainRay(Vector3 point)
         {
             return MainRay(point, -1);
@@ -66,6 +84,30 @@ namespace Painter
             RaycastHit hit;
             Physics.Raycast(Camera.main.ScreenPointToRay(point), out hit, rayLength, mask);
             return hit;
+        }
+        private const float DoublePI = 6.28318530717958f;
+        public static void DrawWrieCircle(Vector3 position, Quaternion rotation, Material mat, float radius = 1.0f)
+        {
+            float angle = DoublePI / 24;
+            GL.PushMatrix();
+            mat.SetPass(0);
+            GL.Begin(GL.LINE_STRIP);
+            for (float i = 0; i <= DoublePI; i += angle)
+            {
+                GL.Vertex(rotation * (new Vector3(Mathf.Cos(i) * radius, Mathf.Sin(i) * radius, 0.0f)) + position);
+            }
+            GL.End();
+            GL.PopMatrix();
+        }
+        public static void DrawWrieLine(Vector3 from, Vector3 to, Material mat)
+        {
+            GL.PushMatrix();
+            mat.SetPass(0);
+            GL.Begin(GL.LINES);
+            GL.Vertex(from);
+            GL.Vertex(to);
+            GL.End();
+            GL.PopMatrix();
         }
     }
 }
